b109f3044e7b2addee0f118a06982bf01ecbbc85
SashaSevastopol <betawho@gmail.com>
2026-07-07 02:33:21 +0300

    Реализация сущности Task

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,26 @@
 public class Task {
+    private String name;
+    private boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false; // по умолчанию задача не выполнена
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return (isDone ? "[x] " : "[] ") + name;
+    }
 }
